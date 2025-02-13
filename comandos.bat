5098a6fae7c4eb54b15ee7d371d275c4b28d6fdf
JuliaSouza04 <aparecidajoyce144@gmail.com>
2025-02-13 14:13:09 -0300

    Update comandos.bat

--- comandos.bat
+++ comandos.bat
@@ -1,7 +1,26 @@
+rem cria uma pasta
 mkdir trabalinho
 mkdir trabalho
-rmdir trabalinho
+
+rem ele entra dentro da pasta e cria outra pasta dentro
 cd trabalho
 mkdir teste
+
+rem cria arquivo texto nome de dev
+echo codigo > dev.txt
+echo ju > senai.txt
+del dev.txt
 cd ..
-mkdir testefora
+
+rem copiar e mover arquivos 
+copy senai.txt ..\trabalinho\senai.txt
+move trabalho\senai.txt trabalinho\senai.txt
+
+rem renomear um arquivo
+ren senai.txt julinda.txt
+
+rem exibir o conteudo do arquivo
+dir
+
+rem ele deleta a pasta
+rmdir /s trabalinho
